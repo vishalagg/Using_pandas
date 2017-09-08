8b3729361350aed54c6f7ec9599e001cfb8d2579
vishalagg <vishalagg1994@gmail.com>
2017-09-08 22:19:19 +0530

    initialised

--- Basics_of_pandas.py
+++ Basics_of_pandas.py
@@ -7,3 +7,8 @@ eg. data = pandas.read_csv("file_name")
 '''
 ****To display first n row of table: use head(n), n= no. ofrows to be displayed,deffaul is 5
 '''
+
+'''
+    To access full list of column space,use coloumn attribute
+    eg. col_name = food_info 
+'''
