8d6c8121727f10192cb2f9a5f5119a83966a8205
vishalagg <vishalagg1994@gmail.com>
2017-09-09 11:06:33 +0530

    Updated

--- Basics_of_pandas.py
+++ Basics_of_pandas.py
@@ -5,10 +5,27 @@ import pandas
 eg. data = pandas.read_csv("file_name")
 '''
 '''
-****To display first n row of table: use head(n), n= no. ofrows to be displayed,deffaul is 5
+****To display first n row of table: use head(n), n= no. of rows to be displayed,default is 5
+eg: data.head() => returns first 5 rows.
+=>  data.head(n) => returns first n rows.
 '''
 
 '''
     To access full list of column space,use coloumn attribute
-    eg. col_name = food_info 
+    eg. col_name = data.columns
+'''
+
+
+'''
+*********loc[] in pandas: As we use bracket notation in Numpy to access the element, in panda we use loc[] to access, here the advantage is that whenever we want to extract a particular row ,let say 0th row, of a table, the loc[0] method will return 0th row along with labels.
+eg:consider the file: "file_name.csv"
+table: name         Age  =>lables
+      vishal        21   => 0th row
+      Mayank        22   => 1th row
+      
+now,first open the file:
+    data = pandas.read_csv("file_name.csv")
+    zero = data.loc[0] => it will return "series object" ,a data structure:
+                          name      Age
+                          vishal     21
 '''
