fd6ea80341021cc00b10e66095a98cf17642ab60
vishalagg <vishalagg1994@gmail.com>
2017-09-14 23:18:07 +0530

    Updated

--- multi_line_charts.py
+++ multi_line_charts.py
@@ -24,4 +24,28 @@ eg: x_values = [0.0, 0.5, 1.0]
 **To extract Month(let say) from date dataset,we can use the pandas.Series.dt accessor:
 e.g:
     month_col['MONTH'] = month_col['DATE'].dt.month
+
+Calling pandas.Series.dt.month returns a Series containing the integer values for each month (e.g. 1 for January, 2 for February, etc.). 
+'''
+'''
+****coloring: in order to show two line graphs in one graph of different colors,we can provide different colors also, by passing c = 'color_name' in plot method.
+
+
+eg:
+fig = plt.figure(figsize=(6,3))
+plt.plot(x_value1,y_value1, c='red')
+plt.plot(x_value2,y_value2, c='blue')
+plt.show()
+
+
+In case of multi lines in single graph,To help remind us which value each line corresponds to, we can add a legend that links each color to the value the line is representing.for this we can pass lable = "line1"(say) in lpot method.
+
+We can create the legend using pyplot.legend and specify its location using the loc parameter:
+    plt.legend(loc='upper left')
+'''
+'''
+title:plt.title('title of chart')
+xlabel: plt.xlabel('x title')
+ylabel: plt.ylabel('y title')
+
 '''
